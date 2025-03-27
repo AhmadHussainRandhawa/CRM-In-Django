15b886e4235c0af302a84e05184163f7cd373dfe
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-03-27 15:49:42 +0500

    configure static files

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import HomePageView
@@ -10,3 +13,5 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
