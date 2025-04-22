c2179ea8f1a4e64e82603bc3c3e1445be1fb62f1
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-04-22 17:16:47 +0500

    add password reset functionality
    using default views: 1. PasswordResetView, 2. PasswordResetDoneView, 3. PasswordResetConfirmView, 4. PasswordResetCompleteView

--- CRM/urls.py
+++ CRM/urls.py
@@ -3,7 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import HomePageView, SignupView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView, 
+    PasswordResetCompleteView,
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +20,11 @@ urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('signup', SignupView.as_view(), name='signup'),
-    
+    path('password-reset', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     path("__reload__/", include("django_browser_reload.urls")),
 
 ]
