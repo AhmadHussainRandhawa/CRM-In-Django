aaf3fc936a3c15a184f89b2f2e646625de567763
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-04-23 17:34:48 +0500

    add unassigned leads to the leadlist context

--- leads/views.py
+++ leads/views.py
@@ -16,13 +16,28 @@ class LeadListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'leads'
 
     def get_queryset(self):
-        if self.request.user.is_organizer:
-            queryset = Lead.objects.filter(organization=self.request.user.userprofile)
+        user = self.request.user
+        if user.is_organizer:
+            queryset = Lead.objects.filter(organization=user.userprofile, agent__isnull=False)
         else:
-            queryset = Lead.objects.filter(organization=self.request.user.agent.organization)
+            queryset = Lead.objects.filter(organization=user.agent.organization)
             # Filter for the agent that is logged in.
-            queryset = queryset.filter(agent__user=self.request.user)
+            queryset = queryset.filter(agent__user=user)
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user = self.request.user
+        if user.is_organizer:
+            queryset = Lead.objects.filter(organization=user.userprofile, agent__isnull=True)
+            context.update({'unassigned_leads': queryset})
+        
+        return context
+
+
+    
+
 
 
 class LeadDetailView(LoginRequiredMixin, generic.DetailView):
